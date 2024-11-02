0cc9edd5ee14d29ffd1729484a1a635c9c6fa3d9
reza <rzxsism80@gmail.com>
2024-11-03 03:10:47 +0330

    statistical analysis feature implemented

--- services/api/revenue_analysis/analyzer.py
+++ services/api/revenue_analysis/analyzer.py
@@ -1,6 +1,11 @@
 from minio import Minio
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructField, StringType, DoubleType, TimestampType, IntegerType, StructType
+from pyspark.sql.functions import to_timestamp, expr
+from pyspark.sql.functions import (
+    col, date_format, round, sum, max, min, count,
+    to_timestamp, expr, floor
+)
+
 from pyspark.sql import functions as func
 from pyspark.sql.dataframe import DataFrame
 
@@ -11,33 +16,24 @@ class Analyzer:
         self.minio_client = minio_client
         self.bucket_name = bucket_name
         self.minio_url = f"s3a://{self.bucket_name}/sms_analysis"
+        self.pay_type = None
 
     def __load_dataset(self, csv_path: str) -> DataFrame:
-        sms_schema = StructType([
-            StructField("ROAMSTATE_519", StringType(), True),
-            StructField("CUST_LOCAL_START_DATE_15", TimestampType(), True),
-            StructField("CDR_ID_1", StringType(), True),
-            StructField("CDR_SUB_ID_2", StringType(), True),
-            StructField("CDR_TYPE_3", StringType(), True),
-            StructField("SPLIT_CDR_REASON_4", StringType(), True),
-            StructField("RECORD_DATE", TimestampType(), True),
-            StructField("PAYTYPE_515", IntegerType(), True),
-            StructField("DEBIT_AMOUNT_42", DoubleType(), True),  # This is in mili rials
-            StructField("SERVICEFLOW_498", StringType(), True),
-            StructField("EVENTSOURCE_CATE_17", StringType(), True),
-            StructField("USAGE_SERVICE_TYPE_19", StringType(), True),
-            StructField("SPECIALNUMBERINDICATOR_534", StringType(), True),
-            StructField("BE_ID_30", StringType(), True),
-            StructField("CALLEDPARTYIMSI_495", StringType(), True),
-            StructField("CALLINGPARTYIMSI_494", StringType(), True)
-        ])
-
-        data_frame = self.spark_client.read.csv(csv_path, schema=sms_schema, header=True)
-        data_frame = data_frame.withColumn("revenue_tomans", func.col("DEBIT_AMOUNT_42") / func.lit(10000))
+        data_frame = self.spark_client.read.csv(csv_path, header=True)
         data_frame = data_frame.withColumn(
+            "RECORD_DATE",
+            to_timestamp("RECORD_DATE", "yyyy/MM/dd HH:mm:ss")
+        ).withColumn(
             "interval_15min",
-            func.date_format(func.date_trunc("15 minutes", "RECORD_DATE"), "HH:mm:ss yyyy/MM/dd")
+            expr(
+                "date_format(date_trunc('MINUTE', RECORD_DATE) - MINUTE(RECORD_DATE) % 15 * \\"
+                "INTERVAL '1' MINUTE, 'yyyy-MM-dd HH:mm')")
         )
+        self.pay_type = self.spark_client.createDataFrame([
+            (0, "Prepaid"),
+            (1, "Postpaid")
+        ], ["PayType", "value"])
+
         return data_frame
 
     def __store_original_debit(self, dataframe: DataFrame):
@@ -48,11 +44,13 @@ class Analyzer:
         )
 
     def __calculate_store_revenue_by_pay_type_interval(self, dataframe: DataFrame):
-        revenue_by_paytype = dataframe.groupBy(
+        revenue_by_paytype = dataframe.groupBy("interval_15min", "PAYTYPE_515").agg(
+            round(sum(col("DEBIT_AMOUNT_42").cast("double")) / 10000, 2).alias("income_tomans")
+        ).join(self.pay_type, col("PAYTYPE_515") == col("PayType")).select(
             "interval_15min",
-            "PAYTYPE_515"
-        ).agg(
-            func.sum("revenue_tomans").alias("revenue")
+            "PAYTYPE_515",
+            "income_tomans",
+            "value"
         ).orderBy("interval_15min", "PAYTYPE_515")
 
         revenue_by_paytype.write.csv(
@@ -61,34 +59,35 @@ class Analyzer:
             mode="overwrite"
         )
 
-    def __max_min_revenue_pay_type_interval(self, dataframe: DataFrame):
-        interval_revenue = dataframe.groupBy(
-            "interval_15min",
-            "PAYTYPE_515"
-        ).agg(
-            func.sum("revenue_tomans").alias("interval_revenue")
-        )
-
-        max_min_by_paytype = interval_revenue.groupBy(
-            "PAYTYPE_515"
-        ).agg(
-            func.max("interval_revenue").alias("max_revenue"),
-            func.min("interval_revenue").alias("min_revenue")
+    def __max_min_income_calculation_interval(self, dataframe: DataFrame):
+        interval_revenue = dataframe.groupBy("interval_15min", "PAYTYPE_515").agg(
+            round(sum(col("DEBIT_AMOUNT_42").cast("double")) / 10000, 2).alias("income_tomans")
+        ).groupBy("PAYTYPE_515").agg(
+            max("income_tomans").alias("max_income"),
+            min("income_tomans").alias("min_income")
+        ).join(self.pay_type, col("PAYTYPE_515") == col("PayType")).select(
+            "PAYTYPE_515",
+            "max_income",
+            "min_income",
+            "value"
         ).orderBy("PAYTYPE_515")
 
-        max_min_by_paytype.write.csv(
+        interval_revenue.write.csv(
             f"{self.minio_url}/max_min_by_pay_type.csv",
             header=True,
             mode="overwrite"
         )
 
-    def __revenue_pay_type_record_interval(self, dataframe: DataFrame):
-        revenue_count_by_pay_type = dataframe.groupBy(
+    def __revenue_pay_type_record_count_interval(self, dataframe: DataFrame):
+        revenue_count_by_pay_type = dataframe.groupBy("interval_15min", "PAYTYPE_515").agg(
+            round(sum(col("DEBIT_AMOUNT_42").cast("double")) / 10000, 2).alias("income_tomans"),
+            count("*").alias("Record_Count")
+        ).join(self.pay_type, col("PAYTYPE_515") == col("PayType")).select(
             "interval_15min",
-            "PAYTYPE_515"
-        ).agg(
-            func.count("*").alias("Record_Count"),
-            func.sum("revenue_tomans").alias("revenue")
+            "PAYTYPE_515",
+            "income_tomans",
+            "Record_Count",
+            "value"
         ).orderBy("interval_15min", "PAYTYPE_515")
 
         revenue_count_by_pay_type.write.csv(
@@ -101,5 +100,5 @@ class Analyzer:
         dataframe = self.__load_dataset(dataset_path)
         self.__store_original_debit(dataframe)
         self.__calculate_store_revenue_by_pay_type_interval(dataframe)
-        self.__max_min_revenue_pay_type_interval(dataframe)
-        self.__revenue_pay_type_record_interval(dataframe)
+        self.__max_min_income_calculation_interval(dataframe)
+        self.__revenue_pay_type_record_count_interval(dataframe)
--- services/api/revenue_analysis/revenue_analyzer_service.py
+++ services/api/revenue_analysis/revenue_analyzer_service.py
@@ -7,5 +7,3 @@ class RevenueAnalyzer:
     def __init__(self):
         self.analyzer = Analyzer(SparkClusterFacade.get_spark(),
                                  SparkClusterFacade.get_minio(), "analysis")
-
-
